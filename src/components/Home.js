e5af03b29e65067d720be3fd41addc22580bcdf0
Raul Camacho <osvaldoracm@gmail.com>
2020-02-13 23:11:19 -0600

    Efecto al Navbar con Jquery

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../css/Home.css'
+
+const Home = () => {
+    return ( 
+<div className="container position">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero necessitatibus impedit, animi distinctio accusantium, illo dolore adipisci praesentium perferendis, corrupti amet! Quia necessitatibus iure repudiandae, beatae molestias deleniti atque unde.</p>
+</div>
+
+     );
+}
+ 
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,63 @@
+import React, { Fragment, Component } from 'react';
+import '../css/Navbar.css';
+
+
+//logo de R&M Asociados
+import logoRmAsociados from '../assets/logo_RM_Asociados.png'
+
+class Nav extends Component {
+    state = {}
+
+    componentDidMount() {
+      const  $= window.$;
+        $(window).scroll(function () {
+            if ($("#menu").offset().top > 56) {
+                $("#menu").css( {"background-color": "#2b2b2cd2"});
+            } else {
+                $("#menu").css({"background-color":''})
+            }
+        })  
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm-12 ">
+                            <nav className="navbar navbar-expand-md fixed-top menu navbar-light navbar-inverse bg-muted" id="menu">
+                                <div className="container">
+                                    <img className="logo" src={logoRmAsociados} alt="logo Rm Asociados" />
+                                    <button type="button" className="navbar-toggler bg-light" data-toggle="collapse" data-target="#nav">
+                                        <span className="navbar-toggler-icon"></span>
+                                    </button>
+                                    <div className="collapse navbar-collapse justify-content-end" id="nav">
+                                        <ul className="navbar-nav">
+                                            <li className="nav-item" >
+                                                <a className="nav-link text-light font-weight-bold px-3" href="!#">Nosotros</a>
+                                            </li>
+                                            <li>
+                                                <a className="nav-link text-light font-weight-bold px-3" href="!#">Hola</a>
+                                            </li>
+                                            <li className="nav-item">
+                                                <a className="nav-link text-light font-weight-bold px-3" href="!#">ABOUT US</a>
+                                            </li>
+                                            <li className="nav-item">
+                                                <a className="nav-link text-light font-weight-bold px-3" href="!#">Contacto</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+
+
+            </Fragment>
+        );
+    }
+}
+
+export default Nav;
+
